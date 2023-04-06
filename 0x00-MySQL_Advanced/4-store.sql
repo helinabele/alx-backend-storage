0956650d13feb5b04edd78b85b2f565132b52796
helina belete <helinabmeaza@gmail.com>
2023-04-06 21:14:24 +0300

    backend storage

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,4 @@
--- sql script that creates a trigger
--- decreases the quantity of an item
-CREATE TRIGGER  decrease_quantity
-AFTER INSERT ON 
-orders FOR EACH ROW
-START 
-UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = New.item_name;
+-- Script that creates a trigger that decreases the
+-- quantity of an item after adding a new order.
+CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
